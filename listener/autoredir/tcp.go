d6ea75a1ac8bb626cd3f69f56f4637a2424d1dcf
opzede <67693411+opzede@users.noreply.github.com>
2023-02-16 17:51:37 +0800

    Update tcp.go

--- listener/autoredir/tcp.go
+++ listener/autoredir/tcp.go
@@ -3,7 +3,8 @@ package autoredir
 import (
 	"net"
 	"net/netip"
-
+	"time"
+	
 	"github.com/Dreamacro/clash/adapter/inbound"
 	C "github.com/Dreamacro/clash/constant"
 	"github.com/Dreamacro/clash/log"
@@ -56,6 +57,7 @@ func (l *Listener) handleRedir(conn net.Conn, in chan<- C.ConnContext) {
 	}
 
 	_ = conn.(*net.TCPConn).SetKeepAlive(true)
+	_ = conn.(*net.TCPConn).SetKeepAlivePeriod(3600 * time.Second)
 
 	in <- inbound.NewSocket(target, conn, C.REDIR, l.additions...)
 }
